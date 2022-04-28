a25c2c72cad024ccfccbc301c2eaa339cff0f1a7
Rodrigo Sandeski <sandeski47@gmail.com>
2022-04-28 03:10:58 -0300

    Finish Example

--- MulticastDelegates/Program.cs
+++ MulticastDelegates/Program.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MulticastDelegates.Services;
+using System;
 
 namespace MulticastDelegates
 {
+    delegate void BinaryNumericOperation(double n1, double n2);
     internal class Program
     {
         static void Main(string[] args)
         {
+            BinaryNumericOperation op = CalculationService.ShowMax;
+            op += CalculationService.ShowSum;
+            op(32, 2);
+            Console.ReadKey();
         }
     }
 }
